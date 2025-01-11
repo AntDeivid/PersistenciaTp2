321bb2881924689a0977df8fb2316c4fc1d4db29
AntDeivid <deividsantos121@gmail.com>
2025-01-11 16:12:19 -0300

    update config

--- src/app/core/db/database.py
+++ src/app/core/db/database.py
@@ -3,10 +3,6 @@ from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass, sessionmaker
 
 from src.app.core.config import settings
 
-
-class Base(DeclarativeBase, MappedAsDataclass):
-    pass
-
 DATABASE_URI = settings.POSTGRES_URI
 DATABASE_PREFIX = settings.POSTGRES_SYNC_PREFIX
 DATABASE_URL = f"{DATABASE_PREFIX}{DATABASE_URI}"
