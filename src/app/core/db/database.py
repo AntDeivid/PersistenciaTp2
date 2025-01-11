31451ff609ccf0ae7fc508f8388e2e6813448dc7
AntDeivid <deividsantos121@gmail.com>
2025-01-11 11:46:06 -0300

    db initial config

---
+++ src/app/core/db/database.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass, sessionmaker
+
+from src.app.core.config import settings
+
+
+class Base(DeclarativeBase, MappedAsDataclass):
+    pass
+
+DATABASE_URI = settings.POSTGRES_URI
+DATABASE_PREFIX = settings.POSTGRES_SYNC_PREFIX
+DATABASE_URL = f"{DATABASE_PREFIX}{DATABASE_URI}"
+
+engine = create_engine(DATABASE_URL, echo=False, future=True)
+
+local_session = sessionmaker(bind=engine, expire_on_commit=False)
+
+def get_db():
+    session = local_session()
+    try:
+        yield session
+    finally:
+        session.close()
